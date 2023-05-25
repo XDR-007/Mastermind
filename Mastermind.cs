2bcc0496a9dec59ba3468a79d1edf8561293610c
Noah Guzik <66749769+XDR-007@users.noreply.github.com>
2023-05-25 09:51:42 -0400

    Final product 2.0

--- Mastermind.cs
+++ Mastermind.cs
@@ -19,7 +19,7 @@ class Mastermind
 
             Random rnd = new Random();
 
-            bool test = false; //set to fals for productioon
+            bool test = false; //set to false for productioon
 
 
             int d1 = rnd.Next(1, 7);   // creates a number between 1 and 6
@@ -50,16 +50,20 @@ class Mastermind
 
             string str_code = string.Join("", random_num_array); //change to random_num_array for final product
 
-            if(debug){
-                  str_code = string.Join("", test_array); //change to random_num_array for final product
+            if (debug)
+            {
+                str_code = string.Join("", test_array); //change to random_num_array for final product
             }
 
-            Console.Write(str_code);
-           // str_code = "3232"; //delete for final product
-
+            // str_code = "3232"; //delete for final product
 
             int i = 0;
 
+            Char[] letters = {};
+
+            int sum_input = 0;
+            
+            int digit = 0;
             StringBuilder score_SB = new StringBuilder("");
             while (i < 12) //change to 12
             {
@@ -74,11 +78,31 @@ class Mastermind
 
                     if (input.Length == 4 && int.TryParse(input, out int res))
                     {
-                        Console.Write("Input valid. Assessing... \n");
-                        valid_input = 'y';
+                        letters = input.ToCharArray();
+                        sum_input = 0;
+                        for(int q = 0; q < 4; q++){
+                            digit = int.Parse(letters[q].ToString());
+                            
+                                
+                            
+                            if ((1 <= digit) && (digit <= 6)){
+                               sum_input++;
+
+                            }
+                        }
+                        if(sum_input == 4){
+                            Console.Write("Input valid. Assessing... \n");
+                               valid_input = 'y';
+                        }else {
+                           Console.Write("Input invalid. Please try again. \n");
+                        valid_input = 'n';
+                        Console.WriteLine("\nEnter four digit integer: ");
+                        input = Console.ReadLine();
+                        }
                     }
                     else
                     {
+
                         Console.Write("Input invalid. Please try again. \n");
                         valid_input = 'n';
                         Console.WriteLine("\nEnter four digit integer: ");
@@ -107,8 +131,8 @@ class Mastermind
                     {
 
                         pluses++;
-                        if(debug)
-                        Console.Write("\n pluses: " + pluses + "\n");
+                        if (debug)
+                            Console.Write("\n pluses: " + pluses + "\n");
                         //score_SB.Append("+");
 
                     }
@@ -119,45 +143,49 @@ class Mastermind
                         //score_SB.Append("-");
 
                         contains++;
-                        if(debug)
-                         Console.Write("\n contains: " + contains + "\n");
-                        
+                        if (debug)
+                            Console.Write("\n contains: " + contains + "\n");
+
                     }
 
-                    if(pluses <= contains){
+                    if (pluses <= contains)
+                    {
                         sum += pluses;
-                    }else if (contains < pluses ){
+                    }
+                    else if (contains < pluses)
+                    {
                         sum += contains;
                     }
 
                 }
 
-                    minuses = sum - pluses;
-                 
-                    
-                    //Console.Write(pluses + " " + minuses);
-
-                    for (int z = 0; z < pluses; z++)
-                    {
-                        score_SB.Append("+");
-
-                    }
-
-                    for (int v = 0; v < minuses; v++)
-                    {
-                        score_SB.Append("-");
-                    }
+                minuses = sum - pluses;
 
 
-                    // Console.Write(anser);
-                
-                Console.Write(score_SB);
-                if(debug){
-                        Console.Write(pluses);
-                        Console.Write(minuses);
-                        Console.Write(contains);
+                //Console.Write(pluses + " " + minuses);
 
-                    }
+                for (int z = 0; z < pluses; z++)
+                {
+                    score_SB.Append("+");
+
+                }
+
+                for (int v = 0; v < minuses; v++)
+                {
+                    score_SB.Append("-");
+                }
+
+
+                // Console.Write(anser);
+
+                Console.Write("Your score: " + score_SB);
+                if (debug)
+                {
+                    Console.Write(pluses);
+                    Console.Write(minuses);
+                    Console.Write(contains);
+
+                }
 
 
                 if (str_code.Equals(input))
@@ -177,7 +205,7 @@ class Mastermind
 
             }
 
-            Console.Write("/n Thanks for playing! Play again? (y/n)");
+            Console.Write("\n Thanks for playing! Play again? (y/n)");
             string play_again_string = "";
             play_again_string = Console.ReadLine();
             play_again = char.Parse(play_again_string);
